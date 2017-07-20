364082ec5623c96b92d3e9f47efe873e3c65bd16
lukanatic <lukanibrahim@gmail.com>
2017-07-20 23:00:10 +0100

    Todo First Commit

---
+++ Todo_List/js/myscript.js
@@ -0,0 +1,8 @@
+ function myTodoList() {
+ 	var item = document.getElementById("todoInput").value;
+ 	var text = document.createTextNode(item);
+ 	var newItem = document.createElement("li");
+ 	newItem.appendChild(text);
+ 	document.getElementById("todoList").appendChild(newItem);
+
+ }
